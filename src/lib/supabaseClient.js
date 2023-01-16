4c2970fb089b1e66ff27ac56d38ecbd567eb0551
sanketssc <49080676+sanketssc@users.noreply.github.com>
2023-01-16 19:55:50 +0530

    removed console log

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,4 +2,3 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY)
-console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
